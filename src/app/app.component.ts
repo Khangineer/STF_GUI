c405fb199a646637f7bd95f1531b48d4520bdc79
Velur <aethillium@gmail.com>
2024-12-23 11:18:22 +0100

    Basic GUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoggedUserDataService } from '../../resources/logged-user-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'STF_GUI';
+  loggedUser : any;
+
+
+  SetLoggedUser(lg : any){
+    this.loggedUser = lg;
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { LoggedUserDataService } from '../../../resources/logged-user-data.service';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  constructor(private loggedUserData : LoggedUserDataService, private appC : AppComponent){
+    this.appC.SetLoggedUser(this.loggedUserData.loggedUser);
+  }
 }
